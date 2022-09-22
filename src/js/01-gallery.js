2d49eeef9810fd289e2b051ca6cedbc06c028179
YuriySamusew <6177385@gmail.com>
2022-09-22 22:58:15 +0300

    test npm committed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img 
+            class="gallery__image" 
+            data-caption="${description}" 
+            src="${preview}" 
+            alt="${description}">
+      </a>
+    </div>   
+    `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 300,
+  animationSpeed: 300,
+});
