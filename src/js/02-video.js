7f4ff87276796807ac9a02a174a4e5258c728a43
YuriySamusew <6177385@gmail.com>
2022-09-25 22:38:10 +0300

    2 таск найдена ошибка переделіваю

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,25 +8,26 @@ const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 
 function playTime(evt) {
   localStorage.save(LOCAL_STORAGE_KEY, Math.floor(evt.seconds));
+  console.log(Math.floor(evt.seconds));
 }
 
 player.on('timeupdate', throttle(playTime, 1000));
 
-const savingTime = JSON.parse(
-  localStorage.save(LOCAL_STORAGE_KEY, Math.floor(evt.seconds))
-);
+// const savingTime = JSON.parse(
+//   localStorage.save(LOCAL_STORAGE_KEY, Math.floor(evt.seconds))
+// );
 
-if (savingTime) {
-  player
-    .setCurrentTime(savingTime.seconds)
-    .then(function (seconds) {})
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          break;
+// if (savingTime) {
+//   player
+//     .setCurrentTime(savingTime.seconds)
+//     .then(function (seconds) {})
+//     .catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           break;
 
-        default:
-          break;
-      }
-    });
-}
+//         default:
+//           break;
+//       }
+//     });
+// }
