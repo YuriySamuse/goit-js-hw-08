cb85cc602bba6e6f8fe23b0d2d573e73b6f7a446
YuriySamusew <6177385@gmail.com>
2022-09-25 20:49:46 +0300

    Task 3 end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerRef = document.querySelector('#vimeo-player');
+const player = new Player(playerRef);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+function playTime(evt) {
+  localStorage.save(LOCAL_STORAGE_KEY, Math.floor(evt.seconds));
+}
+
+player.on('timeupdate', throttle(playTime, 1000));
+
+const savingTime = JSON.parse(
+  localStorage.save(LOCAL_STORAGE_KEY, Math.floor(evt.seconds))
+);
+
+if (savingTime) {
+  player
+    .setCurrentTime(savingTime.seconds)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const LOKAL_STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+initPage();
+
+const onFormInput = evt => {
+  const { name, value } = evt.target;
+
+  try {
+    let saveData = localStorage.getItem(LOKAL_STORAGE_KEY);
+    if (saveData) {
+      saveData = JSON.parse(saveData);
+    } else {
+      saveData = {};
+    }
+
+    saveData[name] = value;
+    const strigifyData = JSON.stringify(saveData);
+    localStorage.setItem(LOKAL_STORAGE_KEY, strigifyData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const throttleOnFormInput = throttle(onFormInput, 500);
+formRef.addEventListener('input', throttleOnFormInput);
+
+function initPage() {
+  const saveData = localStorage.getItem(LOKAL_STORAGE_KEY);
+  if (!saveData) {
+    return;
+  }
+
+  try {
+    const parseData = JSON.parse(saveData);
+    Object.entries(parseData).forEach(([name, value]) => {
+      formRef.elements[name].value = value;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const handleSubmit = e => {
+  e.preventDefault();
+  //   console.log(e.currentTarget);
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  e.currentTarget.reset();
+  localStorage.removeItem(LOKAL_STORAGE_KEY);
+};
+formRef.addEventListener('submit', handleSubmit);
